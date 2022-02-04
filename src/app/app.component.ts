a4039012fc01de70863745c56f5d7bd68a8cfd2d
Crislemen <Crislemen@gmail.com>
2022-02-04 19:56:59 +0000

    Añadido material card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AEUT4_04_Marvel_Films_Cristian';
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && 
+      outlet.activatedRouteData && 
+      outlet.activatedRouteData['animationState'];
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { DetallesPeliculaComponent } from './components/peliculas/detalles-pelicula/detalles-pelicula.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MarvelComponent } from './components/marvel/marvel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,9 @@ import { MarvelComponent } from './components/marvel/marvel.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
